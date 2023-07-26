086be9992f97245e565a0ad2e192158c0bac1fa1
Vadym Yaroshchuk <y9vad9@gmail.com>
2023-07-26 16:51:05 +0200

    refactor: desktop (#54)

---
+++ platforms/desktop/src/main/kotlin/io/timemates/app/Dependencies.kt
@@ -0,0 +1,52 @@
+package io.timemates.app
+
+import app.cash.sqldelight.db.SqlDriver
+import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
+import io.timemates.api.grpc.GrpcTimeMatesRequestsEngine
+import io.timemates.api.grpc.factory.DefaultGrpcEngineBuilder
+import io.timemates.app.authorization.dependencies.AuthorizationDataModule
+import io.timemates.app.authorization.dependencies.screens.ConfirmAuthorizationModule
+import io.timemates.app.authorization.dependencies.screens.StartAuthorizationModule
+import io.timemates.app.core.handler.OnAuthorizationFailedHandler
+import io.timemates.sdk.common.engine.TimeMatesRequestsEngine
+import kotlinx.coroutines.channels.Channel
+import org.koin.core.context.startKoin
+import org.koin.core.qualifier.qualifier
+import org.koin.dsl.module
+import org.koin.ksp.generated.module
+
+/**
+ * Initializes the application dependencies using Koin.
+ *
+ * @param authorizationFailedChannel A [Channel] that will be used to handle authorization failure events.
+ */
+fun initializeDependencies(
+    authorizationFailedChannel: Channel<Unit>
+) {
+    startKoin {
+        val platformModule = module {
+            single<SqlDriver>(qualifier = qualifier("authorization")) {
+                JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY)
+            }
+
+            single<TimeMatesRequestsEngine> {
+                GrpcTimeMatesRequestsEngine(
+                    grpcEngineBuilder = DefaultGrpcEngineBuilder()
+                )
+            }
+
+            single<OnAuthorizationFailedHandler> {
+                OnAuthorizationFailedHandler { exception ->
+                    exception.printStackTrace()
+                    authorizationFailedChannel.trySend(Unit)
+                }
+            }
+        }
+        modules(
+            platformModule,
+            AuthorizationDataModule().module,
+            ConfirmAuthorizationModule().module,
+            StartAuthorizationModule().module,
+        )
+    }
+}
