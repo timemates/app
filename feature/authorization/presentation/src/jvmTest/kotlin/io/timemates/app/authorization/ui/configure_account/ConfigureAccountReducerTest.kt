b36cb2fd489a0d74481eadc4efd71dcceac0f8b3
kotleni <yavarenikya@gmail.com>
2023-07-30 00:45:52 +0300

    tests: implemented for configure_account

---
+++ feature/authorization/presentation/src/jvmTest/kotlin/io/timemates/app/authorization/ui/configure_account/ConfigureAccountReducerTest.kt
@@ -0,0 +1,133 @@
+package io.timemates.app.authorization.ui.configure_account
+
+import io.mockk.every
+import io.mockk.mockk
+import io.timemates.app.authorization.ui.configure_account.mvi.ConfigureAccountReducer
+import io.timemates.app.authorization.ui.configure_account.mvi.ConfigureAccountStateMachine
+import io.timemates.app.authorization.ui.start.mvi.StartAuthorizationStateMachine
+import io.timemates.app.authorization.usecases.CreateNewAccountUseCase
+import io.timemates.app.authorization.validation.UserDescriptionValidator
+import io.timemates.app.authorization.validation.UserNameValidator
+import io.timemates.sdk.authorization.email.types.value.VerificationHash
+import kotlinx.coroutines.test.TestScope
+import io.timemates.app.authorization.ui.configure_account.mvi.ConfigureAccountStateMachine.Effect
+import io.timemates.app.authorization.ui.configure_account.mvi.ConfigureAccountStateMachine.Event
+import io.timemates.app.authorization.ui.configure_account.mvi.ConfigureAccountStateMachine.State
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class ConfigureAccountReducerTest {
+    private val verificationHash: VerificationHash = mockk(relaxed = true)
+    private val createNewAccountUseCase: CreateNewAccountUseCase = mockk()
+    private val userNameValidator: UserNameValidator = mockk()
+    private val userDescriptionValidator: UserDescriptionValidator = mockk()
+
+    private val validName = "kotleni"
+    private val validDescription = "Kotlin developer"
+    private val invalidName = "ko"
+    private val invalidDescription = ""
+
+    private val reducer = ConfigureAccountReducer(
+        verificationHash,
+        createNewAccountUseCase,
+        userNameValidator,
+        userDescriptionValidator,
+        TestScope(),
+    )
+
+    @Test
+    fun `OnDoneClicked with valid name should set appropriate state`() {
+        // GIVEN
+        every { userNameValidator.validate(any()) } returns
+                UserNameValidator.Result.Success
+        every { userDescriptionValidator.validate(any()) } returns
+                UserDescriptionValidator.Result.Success
+
+        // WHEN
+        val result = reducer.reduce(State(name = validName, aboutYou = validDescription), Event.OnDoneClicked) {}
+
+        // THEN
+        assertEquals(
+                expected = State(
+                        name = validName,
+                        aboutYou = validDescription,
+                        isNameSizeInvalid = false,
+                        isAboutYouSizeInvalid = false,
+                        isLoading = true
+                ),
+                actual = result
+        )
+    }
+
+    @Test
+    fun `OnDoneClicked with valid description should set appropriate state`() {
+        // GIVEN
+        every { userNameValidator.validate(any()) } returns
+                UserNameValidator.Result.Success
+        every { userDescriptionValidator.validate(any()) } returns
+                UserDescriptionValidator.Result.Success
+
+        // WHEN
+        val result = reducer.reduce(State(name = validName, aboutYou = validDescription), Event.OnDoneClicked) {}
+
+        // THEN
+        assertEquals(
+                expected = State(
+                        name = validName,
+                        aboutYou = validDescription,
+                        isNameSizeInvalid = false,
+                        isAboutYouSizeInvalid = false,
+                        isLoading = true
+                ),
+                actual = result
+        )
+    }
+
+    @Test
+    fun `OnDoneClicked with invalid name should set appropriate state`() {
+        // GIVEN
+        every { userNameValidator.validate(any()) } returns
+                UserNameValidator.Result.SizeViolation
+        every { userDescriptionValidator.validate(any()) } returns
+                UserDescriptionValidator.Result.Success
+
+        // WHEN
+        val result = reducer.reduce(State(name = invalidName, aboutYou = validDescription), Event.OnDoneClicked) {}
+
+        // THEN
+        assertEquals(
+            expected = State(
+                name = invalidName,
+                aboutYou = validDescription,
+                isNameSizeInvalid = true,
+                isAboutYouSizeInvalid = false,
+                isLoading = false
+            ),
+            actual = result
+        )
+    }
+
+    @Test
+    fun `OnDoneClicked with invalid description should set appropriate state`() {
+        // GIVEN
+        every { userNameValidator.validate(any()) } returns
+                UserNameValidator.Result.Success
+        every { userDescriptionValidator.validate(any()) } returns
+                UserDescriptionValidator.Result.SizeViolation
+
+        // WHEN
+        val result = reducer.reduce(State(name = validName, aboutYou = invalidDescription), Event.OnDoneClicked) {}
+
+        // THEN
+        assertEquals(
+            expected = State(
+                name = validName,
+                aboutYou = invalidDescription,
+                isNameSizeInvalid = false,
+                isAboutYouSizeInvalid = true,
+                isLoading = false
+            ),
+            actual = result
+        )
+    }
+}
