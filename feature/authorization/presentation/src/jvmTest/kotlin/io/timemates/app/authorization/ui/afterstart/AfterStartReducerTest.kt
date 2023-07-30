34d285259ea8b667e7dfa29028e1fa557af086cf
tsyganiimaksym <mixmax4559@gmail.com>
2023-07-30 20:56:17 +0200

    tests (#64): implemented for after start

---
+++ feature/authorization/presentation/src/jvmTest/kotlin/io/timemates/app/authorization/ui/afterstart/AfterStartReducerTest.kt
@@ -0,0 +1,48 @@
+package io.timemates.app.authorization.ui.afterstart
+
+import io.mockk.mockk
+import io.mockk.verify
+import io.timemates.app.authorization.ui.afterstart.mvi.AfterStartReducer
+import io.timemates.app.authorization.ui.afterstart.mvi.AfterStartStateMachine
+import io.timemates.app.foundation.mvi.EmptyState
+import io.timemates.app.foundation.random.nextString
+import io.timemates.sdk.authorization.email.types.value.VerificationHash
+import io.timemates.sdk.common.constructor.createOrThrow
+import kotlin.random.Random
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class AfterStartReducerTest {
+
+    private val verificationHash: VerificationHash = VerificationHash.createOrThrow(Random.nextString(VerificationHash.SIZE))
+    private val sendEffect: (AfterStartStateMachine.Effect) -> Unit = mockk(relaxed = true)
+    private val reducer: AfterStartReducer = AfterStartReducer(verificationHash)
+
+    @Test
+    fun `reducing event NextClicked event should not update the state`() {
+        // GIVEN
+        val state = EmptyState
+        val event = AfterStartStateMachine.Event.NextClicked
+
+        // WHEN
+        val resultState = reducer.reduce(state, event, sendEffect)
+
+        // THEN
+        verify { sendEffect(AfterStartStateMachine.Effect.NavigateToConfirmation(verificationHash)) }
+        assertEquals(state, resultState)
+    }
+
+    @Test
+    fun `reducing event OnChangeEmailClicked event should not update the state`() {
+        // GIVEN
+        val state = EmptyState
+        val event = AfterStartStateMachine.Event.OnChangeEmailClicked
+
+        // WHEN
+        val resultState = reducer.reduce(state, event, sendEffect)
+
+        // THEN
+        verify { sendEffect(AfterStartStateMachine.Effect.OnChangeEmailClicked) }
+        assertEquals(state, resultState)
+    }
+}
