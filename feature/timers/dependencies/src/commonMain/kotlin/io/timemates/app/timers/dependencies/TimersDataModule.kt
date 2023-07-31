2667e6eeaf6e190fc7f8a466e9817fca5c9ae7c5
Vadym Yaroshchuk <y9vad9@gmail.com>
2023-07-31 15:38:16 +0200

    feat (#72): `TimersRepository` implementation

---
+++ feature/timers/dependencies/src/commonMain/kotlin/io/timemates/app/timers/dependencies/TimersDataModule.kt
@@ -0,0 +1,26 @@
+package io.timemates.app.timers.dependencies
+
+import io.timemates.app.users.repositories.TimersRepository
+import io.timemates.sdk.common.engine.TimeMatesRequestsEngine
+import io.timemates.sdk.common.providers.AccessHashProvider
+import io.timemates.sdk.timers.TimersApi
+import io.timemates.sdk.timers.sessions.TimersSessionsApi
+import org.koin.core.annotation.Factory
+import org.koin.core.annotation.Module
+import io.timemates.app.timers.data.TimersRepository as TimersRepositoryImpl
+
+@Module
+class TimersDataModule {
+    @Factory
+    fun timersApi(
+        grpcEngine: TimeMatesRequestsEngine,
+        tokenProvider: AccessHashProvider,
+    ): TimersApi = TimersApi(grpcEngine, tokenProvider)
+
+    @Factory
+    fun timersRepository(
+        timersApi: TimersApi,
+    ): TimersRepository = TimersRepositoryImpl (
+        timersApi, timersApi.members.invites, timersApi.sessions, timersApi.members,
+    )
+}
