d3bdf80e5bdbcbf2bd5f37f8ea3ea5d785c35d91
y9vad9 <y9vad9@gmail.com>
2024-01-05 13:16:59 +0100

    chore: remove unnecessary experimental notation

--- feature/authorization/presentation/src/commonMain/kotlin/io/timemates/app/authorization/ui/configure_account/ConfigureAccountScreen.kt
+++ feature/authorization/presentation/src/commonMain/kotlin/io/timemates/app/authorization/ui/configure_account/ConfigureAccountScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.PersonOutline
 import androidx.compose.material.icons.rounded.ArrowBack
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
@@ -40,7 +39,6 @@ import io.timemates.sdk.users.profile.types.value.UserDescription
 import io.timemates.sdk.users.profile.types.value.UserName
 import kotlinx.coroutines.channels.consumeEach
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ConfigureAccountScreen(
     stateMachine: StateMachine<State, Event, Effect>,
