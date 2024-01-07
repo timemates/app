cbc2be333025dfce0cd7b279476a07347d5869ae
y9vad9 <y9vad9@gmail.com>
2024-01-07 15:12:17 +0100

    chore: set English as default language

--- localization/compose/src/commonMain/kotlin/io/timemates/app/localization/compose/LocalLocalization.kt
+++ localization/compose/src/commonMain/kotlin/io/timemates/app/localization/compose/LocalLocalization.kt
@@ -1,7 +1,8 @@
 package io.timemates.app.localization.compose
 
 import androidx.compose.runtime.staticCompositionLocalOf
+import io.timemates.app.localization.EnglishStrings
 import io.timemates.app.localization.Strings
 import io.timemates.app.localization.UkrainianStrings
 
-val LocalStrings = staticCompositionLocalOf<Strings> { UkrainianStrings }
+val LocalStrings = staticCompositionLocalOf<Strings> { EnglishStrings }
