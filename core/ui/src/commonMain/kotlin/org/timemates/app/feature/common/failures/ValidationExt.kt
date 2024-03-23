18cd22422796b23a2f03938f0e44c9a2e18855c1
y9vad9 <y9vad9@gmail.com>
2024-03-23 17:37:13 +0100

    feat&refactor: better validation with built-in support from SDK

--- core/ui/src/commonMain/kotlin/org/timemates/app/feature/common/failures/ValidationExt.kt
+++ core/ui/src/commonMain/kotlin/org/timemates/app/feature/common/failures/ValidationExt.kt
@@ -1,24 +1,25 @@
 package org.timemates.app.feature.common.failures
 
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import org.timemates.app.localization.Strings
-import org.timemates.app.localization.compose.LocalStrings
 import org.timemates.sdk.common.constructor.CreationFailure
 
 @Stable
 fun CreationFailure.getCommonDisplayMessage(strings: Strings): String {
     return when (this) {
         is CreationFailure.BlankValueFailure -> strings.fieldCannotBeEmpty
-        is CreationFailure.MinValueFailure -> strings.minValueFailure(size)
+        is CreationFailure.MinValueFailure<*> -> strings.minValueFailure(size)
         is CreationFailure.PatternFailure -> strings.patternFailure
-        is CreationFailure.SizeExactFailure -> strings.sizeExactFailure(size)
-        is CreationFailure.SizeRangeFailure -> strings.sizeRangeFailure(range)
+        is CreationFailure.LengthExactFailure -> strings.lengthExactFailure(size)
+        is CreationFailure.LengthRangeFailure -> strings.lengthRangeFailure(range)
+        is CreationFailure.ValueRangeFailure<*> ->
+            strings.valueRangeFailure(range.start, range.endInclusive)
+
+        is CreationFailure.CompoundFailure -> error("stub!")
     }
 }
 
-@Composable
 @Stable
-fun CreationFailure.getCommonDisplayMessage(): String {
-    return getCommonDisplayMessage(LocalStrings.current)
+fun List<CreationFailure>.getRepresentative(strings: Strings): String {
+    return joinToString("\n") { it.getCommonDisplayMessage(strings) }
 }
