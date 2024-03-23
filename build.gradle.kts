62ebd802a6ecc014dd1ee5f826a2e81fcc255b92
y9vad9 <y9vad9@gmail.com>
2024-03-23 17:34:44 +0100

    feat: compose reports & compose stability config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
-import org.jetbrains.kotlin.gradle.dsl.kotlinExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
     alias(libs.plugins.ksp) apply false
@@ -17,7 +17,22 @@ group = "org.timemates.app"
 allprojects {
     tasks.withType<KotlinCompile> {
         kotlinOptions {
-            freeCompilerArgs = listOf("-Xskip-prerelease-check")
+            freeCompilerArgs += listOf("-Xskip-prerelease-check")
+
+            val composeReportsDir = "compose_reports"
+
+            freeCompilerArgs += listOf(
+                "-P",
+                "plugin:androidx.compose.compiler.plugins.kotlin:stabilityConfigurationPath=$rootDir/stability-config.txt"
+            )
+            freeCompilerArgs += listOf(
+                "-P",
+                "plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=" +
+                    project.layout.buildDirectory.get().dir(composeReportsDir).asFile.absolutePath,
+                "-P",
+                "plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=" +
+                    project.layout.buildDirectory.get().dir(composeReportsDir).asFile.absolutePath,
+            )
         }
     }
 }
